771b974494290524507b18350530fb9962eff7af
JinMuGo <jingmuio@gmail.com>
2024-05-12 11:14:16 +0900

    add history-substring-search

--- zsh/plugins/index.zsh
+++ zsh/plugins/index.zsh
@@ -9,6 +9,8 @@ source $ZSH_PLUGINS/zsh-docker/zsh-docker.plugin.zsh
 source $ZSH_PLUGINS/zsh-docker-compose/docker-compose.plugin.zsh
 source $ZSH_PLUGINS/zsh-fzf/fzf.plugin.zsh
 source $ZSH_PLUGINS/zsh-history-substring-search/zsh-history-substring-search.zsh
+source $ZSH_PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
 
 fpath=($ZSH_PLUGINS/zsh-completions/src $fpath)
 [ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
